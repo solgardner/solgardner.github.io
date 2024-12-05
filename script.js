4492513566b8ae8bd758939e9a3a2d0216dc888b
Ali <54851750+achaghani21@users.noreply.github.com>
2024-12-05 15:35:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Add click events for arrows
-    if (instances.length > 0) {
-        const carousel = instances[0]; // Assuming only one carousel on the page
+    const carousel = instances[0]; // Assuming only one carousel on the page
 
-        document.querySelector('.prev-arrow').addEventListener('click', function () {
-            carousel.prev();
-        });
+    document.querySelector('.prev-arrow').addEventListener('click', function () {
+        carousel.prev();
+    });
 
-        document.querySelector('.next-arrow').addEventListener('click', function () {
-            carousel.next();
-        });
-    }
-});
-
-// Ensure proper layout after all content (including images) has loaded
-window.addEventListener('load', function () {
-    var elems = document.querySelectorAll('.carousel');
-    if (elems.length > 0) {
-        // Re-initialize carousel to address potential layout issues
-        M.Carousel.init(elems, {
-            fullWidth: true,
-            indicators: true
-        });
-    }
+    document.querySelector('.next-arrow').addEventListener('click', function () {
+        carousel.next();
+    });
 });
